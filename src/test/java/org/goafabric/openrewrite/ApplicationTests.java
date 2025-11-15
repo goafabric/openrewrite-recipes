4388be0d98dbed705e8b8b6fcc5370deb2ff889a
Andreas Mautsch <amautsch>
2025-11-15 11:27:16 +0100

    initial import

--- src/test/java/org/goafabric/openrewrite/ApplicationTests.java
+++ src/test/java/org/goafabric/openrewrite/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OpenrewriteApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
